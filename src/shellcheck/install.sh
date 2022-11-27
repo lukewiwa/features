536e956289376473150258e5a237a8ee7c406684
lukewiwa <luke.wiwa@gmail.com>
2022-11-27 02:22:37 +0000

    Use tmp directory to install shellcheck

--- src/shellcheck/install.sh
+++ src/shellcheck/install.sh
@@ -15,6 +15,14 @@ else
 fi
 
 
+TMP_DIR=/tmp/shellcheck-tmp
+
+mkdir --parents $TMP_DIR
+pushd $TMP_DIR
+
 curl -sSL \
     "https://github.com/koalaman/shellcheck/releases/download/${SC_VERSION}/shellcheck-${SC_VERSION}.linux.$(uname -m).tar.xz" | tar -xJv
 cp "shellcheck-${SC_VERSION}/shellcheck" /usr/local/bin/
+
+popd
+rm -rf $TMP_DIR
